94c6d825f858a081dd90286153e32bcb09aa0300
ikrambich <ikram.bichbich8@gmail.com>
2024-08-16 00:28:00 +0100

    Added some music

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,9 +1,11 @@
 extends Area2D
 @onready var timer = $Timer
+@onready var sound_player = $AudioStreamPlayer  # Get the AudioStreamPlayer node
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
-		print("You died!")
+		sound_player.play()
+		print("You diedd!")
 		Engine.time_scale = 0.5
 		body.die() # Call the die function on the player
 		body.get_node("CollisionShape2D").queue_free()
